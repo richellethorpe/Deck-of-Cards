824fe2fc5530774b7f468f3dddb9fb835889ad0d
Richelle <richelle.ak@gmail.com>
2022-11-18 12:32:58 -0800

    add UI logic to append loop values to doc

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-const suits = ['clubs', 'hearts', 'spades', 'diamonds']
-const increment = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', 'Jack', 'Queen', 'King']
+//for each suit we want to add a corresponding increment
 
+const cardDeck = [];
+const suits = ['clubs', 'hearts', 'spades', 'diamonds'];
+const increments = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', 'Jack', 'Queen', 'King'];
+
+const ul = document.getElementById("list");
+suits.forEach(suit => {
+  increments.forEach(increment => {
+    cardDeck.push(increment.concat(" of " + suit))
+  })
+})
+
+cardDeck.forEach(element => {
+  const li = document.createElement('li');
+  li.append(element);
+  ul.append(li);
+})
